c3397e16c930b5d568cd26b11c7d0b9702b2ff52
hasan99999id-png <hasan99999id@gmail.com>
2025-12-18 15:22:32 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener("fetch", event => {});
